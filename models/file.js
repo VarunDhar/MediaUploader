d6e436fe920c1f82a369a7423d08be686b49be46
VarunDhar <85066986+VarunDhar@users.noreply.github.com>
2023-10-11 15:51:14 +0530

    Update file.js
    Added PostMiddleware and Nodemailer functionality, 
    sending mail to the uploader notifying them about successful upload.

--- models/file.js
+++ models/file.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -15,5 +17,31 @@ const fileSchema = new mongoose.Schema({
         type:String
     }
 });
+
+fileSchema.post('save',async function(doc){
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            // secure: true,
+            auth: {
+              user: `${process.env.MAIL_ID}`,
+              pass: `${process.env.MAIL_PASS}`,
+            },
+          });
+
+          console.log(process.env.MAIL_HOST);
+          const info = await transporter.sendMail({
+            from: `${process.env.MAIL_ID}`,
+            to: doc.email, // list of receivers
+            subject: "File uploaded successfully!", // Subject line
+            text: "This is to inform you that your file has been uploaded successfully!😀", // plain text body
+            html: `<b>File has been uploaded successfully!😀</b><br>File link: ${doc.fileUrl}`, // html body
+          });
+        
+          console.log("Message sent: %s", info.messageId);
+    } catch (error) {
+        console.log("Error sending mail: ",error);
+    }
+})
 
 module.exports = mongoose.model("File",fileSchema);
